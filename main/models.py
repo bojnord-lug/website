5a7ce220e52c6af9f323269ad36384213ca52084
Milad Hazrati <miladhazrati75@gmail.com>
2020-01-19 11:32:14 +0330

    category model added foreign key for news model

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_jalali.db import models as jmodels
 
+
 class PostImage(models.Model):
     title = models.CharField(max_length=20)
     image = models.ImageField()
@@ -13,18 +14,21 @@ class PostImage(models.Model):
 class Event(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    location = models.TextField() # where is it
+    location = models.TextField()  # where is it
     date = jmodels.jDateField()  # when is the event
     unitl = jmodels.jDateField()  # when singup for the event finishes
 
     def __str__(self):
         return self.title
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profession = models.CharField(max_length=20) # to specify the profession of the user 
-                                                 # for instance SysAdmin, Programmer and etc.
+    # to specify the profession of the user
+    profession = models.CharField(max_length=20)
+    # for instance SysAdmin, Programmer and etc.
     profile_picture = models.ImageField(upload_to='profile_pictures/')
+
 
 class SubComment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -32,6 +36,7 @@ class SubComment(models.Model):
 
     def __str__(self):
         return self.text
+
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -45,13 +50,17 @@ class Comment(models.Model):
     def __str__(self):
         return self.user.first_name
 
+
 class news(models.Model):
-    title=models.CharField(max_length=50,blank=False,null=False)
-    text=models.TextField()
-    image=models.ImageField(upload_to="image")
-    date=jmodels.jDateField()
+    title = models.CharField(max_length=50, blank=False, null=False)
+    text = models.TextField()
+    image = models.ImageField(upload_to="image")
+    date = jmodels.jDateField()
+    category = models.ForeignKey(models.category)
 
     def __str__(self):
         return "{}".format(self.title)
 
-   
+
+class category(models.Model):
+    title = models.CharField(max_length=50, blank=False, null=False)
