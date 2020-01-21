9f5a736d423e9dc93c2b6edf462431da02d915b9
Milad Hazrati <miladhazrati75@gmail.com>
2020-01-21 15:53:29 +0330

    search almost ok

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search', views.search, name='search'),
 
-    path("profile/", views.profile, name='profile'),
+    path("profile", views.profile, name='profile'),
 
-    path("contact/", views.contact, name='contact'),
+    path("contact", views.contact, name='contact'),
 
     path("category", views.category, name='category'),
 
