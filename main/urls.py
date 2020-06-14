75bee0aed42ac9af8f2594e1af42e029e1243da4
Mehrshad <mehrshadfiroozian@gmail.com>
2020-06-14 19:44:18 +0430

    Add /new-post to urls

--- main/urls.py
+++ main/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path('reset-password', PasswordResetView.as_view(), name='password_reset'),
     path('reset-password/done', PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('reset-password/confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(template_name='password-reset.htm', success_url='/?password-reset=true'), name='password_reset_confirm'),
-    path('reset-password/complete', PasswordResetCompleteView.as_view(), name="password_reset_complete")
+    path('reset-password/complete', PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('new-post', views.new_post)
 ]
 
 if settings.DEBUG:
